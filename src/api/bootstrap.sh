fdbaeb9bccaba9eb50634cdd56ba2098d3a50454
Jack Haynes <jackhaynes94@gmail.com>
2018-12-29 23:58:45 +0000

    Added check for venv dir in bootstrap script.

--- src/api/bootstrap.sh
+++ src/api/bootstrap.sh
@@ -4,5 +4,10 @@ export FLASK_APP=run.py
 export VENV_DIR=../../venv
 
 # Start instance of Flask dev server inside virtual environment.
-source $VENV_DIR/bin/activate
-flask run
+if [ -d "$VENV_DIR" ]; 
+    then
+        source $VENV_DIR/bin/activate
+        flask run
+    else
+        echo "A virtual environment could not be found at $VENV_DIR"
+fi
