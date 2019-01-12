9bdb9a901ad8cdbf99f0e4d3bdb734f8037c17f4
Jack Haynes <jackhaynes94@gmail.com>
2019-01-12 21:34:46 +0000

    Moved database out to repo root to seperate from api.

--- api/api/config.py
+++ api/api/config.py
@@ -1,1 +1,24 @@
-test2 = "hello"
+import os
+
+class Config(object):
+    # Paths.
+    APP_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    # Database.
+    DB_NAME = os.environ['DB_NAME']
+    DB_SERVER = os.environ['DB_SERVER']
+    DB_USER = os.environ['DB_USER']
+    DB_PASSWORD = os.environ['DB_PASSWORD']
+
+class ProdConfig(Config):
+    ENV = 'prod'
+    DEBUG = False
+
+class DevConfig(Config):
+    ENV = 'dev'
+    DEBUG = True
+
+class TestConfig(Config):
+    ENV = 'test'
+    DEBUG = True
+    TESTING = True
